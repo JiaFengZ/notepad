62c49a5a43a1ff1c9795c3721b26cda0fe08aa62
jiafengZ <1040185058@qq.com>
2018-08-08 08:13:14 +0800

    feat: 编辑书签操作

--- lib/components/editMark.dart
+++ lib/components/editMark.dart
@@ -10,33 +10,48 @@ class EditMark extends StatefulWidget {
 class _EditMarkState extends State<EditMark> {
   static List<Map> _marks = [{
     'name': '笔记',
-    'id': '1'
+    'id': '1',
+    'isNew': false,
+    'toRemoveed': false
   }, {
     'name': '旅游',
-    'id': '2'
+    'id': '2',
+    'isNew': false,
+    'toRemoveed': false
   }, {
     'name': '生活',
-    'id': '3'
+    'id': '3',
+    'isNew': false,
+    'toRemoveed': false
   }, {
     'name': '工作',
-    'id': '4'
+    'id': '4',
+    'isNew': false,
+    'toRemoveed': false
   }, {
     'name': '学习',
-    'id': '5'
+    'id': '5',
+    'isNew': false,
+    'toRemoveed': false
   }];
 
-  List<Widget> _items;
+  List<Map> _selectedItemList = [];
+
+  bool _removeAble = false;
+
 
   @override
-  void initState() {
-    super.initState();
-    _items = _buildList();
-  }
-
   Widget build(BuildContext context) {
+    List<Widget> _items = _buildList();
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('编辑书签'),
+        leading: _removeAble ?
+          new IconButton(
+              icon: new Icon(Icons.close),
+              onPressed: _cancelRemove
+          ) :
+          new BackButton(),
+        title: new Text(_removeAble ? '选择书签' : '编辑书签'),
         automaticallyImplyLeading: true,
       ),
       body: new ListView(
@@ -45,7 +60,7 @@ class _EditMarkState extends State<EditMark> {
       ),
       persistentFooterButtons: <Widget>[
         new IconButton(icon: new Icon(Icons.add), onPressed: _add),
-        new IconButton(icon: new Icon(Icons.delete), onPressed: null)
+        new IconButton(icon: new Icon(Icons.delete), onPressed: _enabledRemove)
       ],
     );
   }
@@ -55,36 +70,67 @@ class _EditMarkState extends State<EditMark> {
   }
 
   Widget _buildListItem(Map item) {
+    final alreadySelected = _selectedItemList.contains(item);
     return new ListTile(
-      //title: new Text(item['name']),
       leading: new Icon(Icons.bookmark),
       title: new TextField(
         maxLines: 1,
-        autofocus: false,
+        autofocus: true,
         decoration: null,
         controller: new TextEditingController(text: item['name']),
-      )
+      ),
+      trailing: item['isNew'] ? new ButtonBar(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new IconButton(icon: new Icon(Icons.check), onPressed: null),
+          new IconButton(icon: new Icon(Icons.close), onPressed: null)
+        ],
+      ) : _removeAble ? new ButtonBar(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new IconButton(
+              icon: new Icon(
+                  alreadySelected ? Icons.check_box : Icons.check_box_outline_blank,
+                  color: alreadySelected ? Colors.red : null),
+              onPressed: () {
+                setState(() {
+                  if (alreadySelected) {
+                    _selectedItemList.remove(item);
+                  } else {
+                    _selectedItemList.add(item);
+                  }
+                });
+              }
+          )
+        ],
+      ) : null,
     );
   }
 
   void _add() {
     setState(() {
-      _items.add(
-          new ListTile(
-            leading: new Icon(Icons.bookmark),
-            title: new TextField(
-              maxLines: 1,
-              autofocus: true,
-              decoration: null,
-            ),
-            trailing: new ButtonBar(
-              children: <Widget>[
-                new IconButton(icon: new Icon(Icons.check), onPressed: null),
-                new IconButton(icon: new Icon(Icons.close), onPressed: null)
-              ],
-            ),
-          )
+      final int len = _marks.length;
+      _marks.insert(
+          0,
+          {
+            'name': '',
+            'id': len.toString(),
+            'isNew': true,
+            'toRemoveed': false
+          }
       );
+    });
+  }
+
+  void _enabledRemove() {
+    setState(() {
+      _removeAble = true;
+    });
+  }
+
+  void _cancelRemove() {
+    setState(() {
+      _removeAble = false;
     });
   }
 }
